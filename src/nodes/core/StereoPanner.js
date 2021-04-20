05fbdd7e25ef01d1537da781e09f201cf75d1474
KJ Shanks <kyle.j.shanks@gmail.com>
2021-04-20 02:26:44 -0400

    Add StereoPanner and EQ2 nodes

---
+++ src/nodes/core/StereoPanner.js
@@ -0,0 +1,24 @@
+import DotAudioNode from '../DotAudioNode.js'
+
+class StereoPanner extends DotAudioNode {
+    constructor(AC) {
+        super(AC)
+        this.name = 'StereoPanner'
+        this.panner = this.AC.createStereoPanner()
+
+        this.params = {
+            pan: this.panner.pan,
+        }
+    }
+
+    // Getters
+    getInputs = () => [this.panner]
+    getOutputs = () => [this.panner]
+
+    getPan = () => this.params.pan.value
+
+    // Setters
+    setPan = (val, time) => this._timeUpdate(this.params.pan, val, time)
+}
+
+export default StereoPanner
---
+++ src/nodes/effects/EQ2.js
@@ -0,0 +1,42 @@
+import DotAudioNode from '../DotAudioNode.js'
+import Filter from '../core/Filter.js'
+
+class EQ2 extends DotAudioNode {
+    constructor(AC) {
+        super(AC)
+        this.name = 'EQ2'
+        this.low = new Filter(this.AC)
+        this.high = new Filter(this.AC)
+
+        this.low.setType('lowshelf')
+        this.low.setFreq(320.0)
+        this.high.setType('highshelf')
+        this.high.setFreq(3200.0)
+
+        this.params = {
+            lowFreq: this.low.getParams().frequency,
+            lowGain: this.low.getParams().gain,
+            highFreq: this.high.getParams().frequency,
+            highGain: this.high.getParams().gain,
+        }
+
+        this.low.connect(this.high)
+    }
+
+    // Getters
+    getInputs = () => [this.low]
+    getOutputs = () => [this.high]
+
+    getLowFreq = () => this.params.lowFreq.value
+    getLowGain = () => this.params.lowGain.value
+    getHighFreq = () => this.params.highFreq.value
+    getHighGain = () => this.params.highGain.value
+
+    // Setters
+    setLowGain = (val, time) => this.low.setGain(val, time)
+    setLowFreq = (val, time) => this.low.setFreq(val, time)
+    setHighGain = (val, time) => this.high.setGain(val, time)
+    setHighFreq = (val, time) => this.high.setFreq(val, time)
+}
+
+export default EQ2
