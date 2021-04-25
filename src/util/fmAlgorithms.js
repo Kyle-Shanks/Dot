ebb8f3467db99f6221215eff6edbdc09b052c023
KJ Shanks <kyle.j.shanks@gmail.com>
2021-04-25 15:54:45 -0400

    Add feedback algorithms for FMSynth

--- src/util/fmAlgorithms.js
+++ src/util/fmAlgorithms.js
@@ -1,6 +1,7 @@
 const disconnectAll = (arr) => arr.forEach(mod => mod.disconnect())
 
 const fmAlgorithms = [
+    // - Standard Algorithms -
     // A > B > C > D => Out
     (a, b, c, d, out) => {
         disconnectAll([a, b, c, d])
@@ -135,6 +136,114 @@ const fmAlgorithms = [
         d.connect(out)
 
         return 'A + B + C + D => Out'
+    },
+
+    // - Feedback Algorithms -
+    // [A > A] > B > C > D => Out
+    (a, b, c, d, out) => {
+        disconnectAll([a, b, c, d])
+
+        a.connect(a.getParams().frequency)
+        a.connect(b.getParams().frequency)
+        b.connect(c.getParams().frequency)
+        c.connect(d.getParams().frequency)
+        d.connect(out)
+
+        return '[A > A] > B > C > D => Out'
+    },
+
+    // [[A > A] + B] > C > D => Out
+    (a, b, c, d, out) => {
+        disconnectAll([a, b, c, d])
+
+        a.connect(a.getParams().frequency)
+        a.connect(c.getParams().frequency)
+        b.connect(c.getParams().frequency)
+        c.connect(d.getParams().frequency)
+        d.connect(out)
+
+        return '[[A > A] + B] > C > D => Out'
+    },
+
+    // [[A > A] + B > C] > D => Out
+    (a, b, c, d, out) => {
+        disconnectAll([a, b, c, d])
+
+        a.connect(a.getParams().frequency)
+        a.connect(d.getParams().frequency)
+        b.connect(c.getParams().frequency)
+        c.connect(d.getParams().frequency)
+        d.connect(out)
+
+        return '[[A > A] + B > C] > D => Out'
+    },
+
+    // [[A > A] > B + C] > D => Out
+    (a, b, c, d, out) => {
+        disconnectAll([a, b, c, d])
+
+        a.connect(a.getParams().frequency)
+        a.connect(b.getParams().frequency)
+        b.connect(d.getParams().frequency)
+        c.connect(d.getParams().frequency)
+        d.connect(out)
+
+        return '[[A > A] > B + C] > D => Out'
+    },
+
+    // [A > A] > B + [C > C] > D => Out
+    (a, b, c, d, out) => {
+        disconnectAll([a, b, c, d])
+
+        a.connect(a.getParams().frequency)
+        a.connect(b.getParams().frequency)
+        b.connect(out)
+        c.connect(c.getParams().frequency)
+        c.connect(d.getParams().frequency)
+        d.connect(out)
+
+        return '[A > A] > B + [C > C] > D => Out'
+    },
+
+    // [A > A > B] + [A > A > C] + [A > A > D] => Out
+    (a, b, c, d, out) => {
+        disconnectAll([a, b, c, d])
+
+        a.connect(a.getParams().frequency)
+        a.connect(b.getParams().frequency)
+        a.connect(c.getParams().frequency)
+        a.connect(d.getParams().frequency)
+        b.connect(out)
+        c.connect(out)
+        d.connect(out)
+
+        return '[A > A > B] + [A > A > C] + [A > A > D] => Out'
+    },
+
+    // [A > A > B] + C + D => Out
+    (a, b, c, d, out) => {
+        disconnectAll([a, b, c, d])
+
+        a.connect(a.getParams().frequency)
+        a.connect(b.getParams().frequency)
+        b.connect(out)
+        c.connect(out)
+        d.connect(out)
+
+        return '[A > A > B] + C + D => Out'
+    },
+
+    // [A > A] + B + C + D => Out
+    (a, b, c, d, out) => {
+        disconnectAll([a, b, c, d])
+
+        a.connect(a.getParams().frequency)
+        a.connect(out)
+        b.connect(out)
+        c.connect(out)
+        d.connect(out)
+
+        return '[A > A] + B + C + D => Out'
     },
 ]
 
